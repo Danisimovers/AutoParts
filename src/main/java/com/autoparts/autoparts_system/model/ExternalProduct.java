863f6c88ed16303e3ea6084ed69c9e095425ec8a
Danil_Dimitrov <danil6608@mail.ru>
2026-05-06 21:06:22 +0300

    feat: добавлена модель ExternalProduct для товаров поставщиков

---
+++ src/main/java/com/autoparts/autoparts_system/model/ExternalProduct.java
@@ -0,0 +1,19 @@
+package com.autoparts.autoparts_system.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExternalProduct {
+    private Long id;
+    private String factoryNumber;
+    private String name;
+    private String producer;
+    private Double price;
+    private Integer stock;
+    private String delivery;
+    private String supplierName;
+}
